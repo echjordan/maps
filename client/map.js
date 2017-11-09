5ff40b516ccbeec988485d5a7c0bc99046e00885
Emily Jordan <emily.h.jordan@gmail.com>
2017-11-09 12:13:48 -0500

    map rendering init

--- client/map.js
+++ client/map.js
@@ -1,25 +1,30 @@
-import getHotspots, {putWifiData} from './hotspots'
-import getPayphones, {putPayphoneData} from './payphones'
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 2,
+//     center: new google.maps.LatLng(2.8, -187.3),
+//     mapTypeId: 'terrain'
+//   });
 
-var map;
+//   // Create a <script> tag and set the USGS URL as the source.
+//   var script = document.createElement('script');
+//   // This example uses a local copy of the GeoJSON stored at
+//   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+//   script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+//   document.getElementsByTagName('head')[0].appendChild(script);
+// }
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: new google.maps.LatLng(40.7589, -73.9851),
-    mapTypeId: 'terrain'
-  });
-}
+// // Loop through the results array and place a marker for each
+// // set of coordinates.
+// window.eqfeed_callback = function (results) {
+//   for (var i = 0; i < results.features.length; i++) {
+//     var coords = results.features[i].geometry.coordinates;
+//     var latLng = new google.maps.LatLng(coords[1], coords[0]);
+//     var marker = new google.maps.Marker({
+//       position: latLng,
+//       map: map
+//     });
+//   }
+// }
 
 
-function eqfeed_callback_wifi(results) {
-  // ABSTRACT AWAY GRABBING DATA
-  putWifiData(results)
-
-}
-
-// 40.7589, -73.9851 Times Square
-
-function eqfeed_callback_phone(results) {
-  putPayphoneData(results)
-}
