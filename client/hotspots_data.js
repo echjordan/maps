88b0fa4c98140ee1aa8f0a335af161c2fc6213de
Emily Jordan <emily.h.jordan@gmail.com>
2017-11-10 22:44:23 -0500

    removed unused files for deploy

--- client/hotspots_data.js
+++
@@ -1,7 +0,0 @@
-// const hotspotData = () => {
-//   return axios.get('https://data.cityofnewyork.us/api/views/varh-9tsp/rows.json?accessType=DOWNLOAD')
-//     .then((res) => res.data.data)
-//     .catch(err => console.error(err))
-// }
-
-// export default hotspotData
--- client/links_data.js
+++
@@ -1,7 +0,0 @@
-// const linkData = () => {
-//   return axios.get("https://data.cityofnewyork.us/resource/3ktt-gd74.json")
-//   .then(res => res.data)
-//   .catch(err => console.error(err))
-// }
-
-// export default linkData
